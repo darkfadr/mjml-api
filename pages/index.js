3c381a61eedd48cefffa3e60f2f4cb30947f4d74
Ashley <ashley_narcisseaustin@ultimatesoftware.com>
2019-07-07 14:27:11 -0400

    feat: added terminal stlying on landing page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import styled from '@emotion/styled';
-import { Pane } from 'evergreen-ui'
+import { Card } from 'evergreen-ui'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-// import sh from 'react-syntax-highlighter/dist/esm/languages/prism/bash';
-import night from 'react-syntax-highlighter/dist/esm/styles/prism/vs-dark';
+import night from 'react-syntax-highlighter/dist/esm/styles/prism/tomorrow';
  
 const Wrapper = styled.main`
   display: flex;
@@ -18,7 +17,28 @@ const Wrapper = styled.main`
     font-size: 3em;
     font-weight: 300;
   }
+
+  & pre {
+    ${'' /* border-bottom-left-radius: 10px;
+    border-bottom--right-radius: 10px; */}
+    margin-top: 0px !important;
+    margin-bottom: 0px !important;
+  }
 `;
+
+const Terminal = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background: #bdb9b9;
+  padding:6px;
+`;
+
+const CLI = styled(SyntaxHighlighter)`
+  border-bottom-radius: 10px;
+  margin-bottom: 0px;
+`; 
 
 const Footer = styled.footer`
   margin-top: auto;
@@ -26,8 +46,16 @@ const Footer = styled.footer`
   background: linear-gradient(180deg,#f46d4c 0,#e4505d);
 `;
 
+const Action = styled.div`
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  margin-left: 5px;
+  background: ${({color}) => color};
+`;
+
 const code = `
-$ curl -X POST "https://mjml.now.sh/render" \ 
+$ curl -X POST "https://mjml.now.sh/render" \\ 
   -d '{ 
     "mjml":"<mjml><mj-body><mj-container><mj-section><mj-column><mj-text>Hello World</mj-text></mj-column></mj-section></mj-container></mj-body></mjml>" 
   }'
@@ -38,10 +66,18 @@ export default () => (
     {/* <img src="https://mjml.io/assets/img/logo-white-small.png" /> */}
     <img src="https://mjml.io/documentation/images/logo.png" />
     <h1>Responsive Email as a Service</h1>
-    <Pane width={900}>
-      <SyntaxHighlighter language="javascript" style={night}>
+    <Card width={900} elevation={3} style={{overflow: 'hidden'}}>
+      <Terminal>
+        <Action color="#ea3838"/>
+        <Action color="#fffd62"/>
+        <Action color="#39fb00"/>
+      </Terminal>
+      <CLI 
+        style={night}
+        wrapLines={true}
+        language="javascript">
         {code}
-      </SyntaxHighlighter>
-    </Pane>
+      </CLI>
+    </Card>
   </Wrapper>
 );
