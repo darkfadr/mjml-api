1cd53dc6e143d6707d51efe371ed8fde8296ef4d
Ashley <ashley_narcisseaustin@ultimatesoftware.com>
2019-07-07 13:30:47 -0400

    doc: added initial documentation to README and landing page

---
+++ README.md
@@ -0,0 +1,2 @@
+Reese: REaaS - Responsive Email as a Service
+> Programmatically build responsive emails right in your app by integrating the REaaS API, it's free to use!
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,47 @@
-import Link from "next/link";
-import Header from "../components/header";
+import styled from '@emotion/styled';
+import { Pane } from 'evergreen-ui'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import sh from 'react-syntax-highlighter/dist/esm/languages/prism/bash';
+import night from 'react-syntax-highlighter/dist/esm/styles/prism/vs-dark';
+ 
+const Wrapper = styled.main`
+  display: flex;
+  height: 95vh;
+  width: 95vw;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #f46d4c;
+  font-family: 'Montserrat', sans-serif;
+
+  & h1 {
+    font-size: 3em;
+    font-weight: 300;
+  }
+`;
+
+const Footer = styled.footer`
+  margin-top: auto;
+  height: 300px;
+  background: linear-gradient(180deg,#f46d4c 0,#e4505d);
+`;
+
+const code = `
+$ curl -X POST "https://mjml.now.sh/render" \ 
+  -d '{ 
+    "mjml":"<mjml><mj-body><mj-container><mj-section><mj-column><mj-text>Hello World</mj-text></mj-column></mj-section></mj-container></mj-body></mjml>" 
+  }'
+`;
 
 export default () => (
-  <main>
-    <Header />
-    <section>
-      Docs incoming
-    </section>
-  </main>
+  <Wrapper>
+    {/* <img src="https://mjml.io/assets/img/logo-white-small.png" /> */}
+    <img src="https://mjml.io/documentation/images/logo.png" />
+    <h1>Responsive Email as a Service</h1>
+    <Pane width={900}>
+      <SyntaxHighlighter language="javascript" style={night}>
+        {code}
+      </SyntaxHighlighter>
+    </Pane>
+  </Wrapper>
 );
